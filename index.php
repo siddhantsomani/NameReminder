bc055a88f87e1c4b6d1ee87be0a3ac047ae1a65f
siddhantsomani <siddhantsomani@gmail.com>
2016-09-04 23:37:07 +0530

    fix

--- index.php
+++ index.php
@@ -7,15 +7,6 @@
   </head>
   <!--<body style="background-image: url(img/bgimage.jpg);background-size: cover;">-->
   <body>
-
-<!-- <form action = "saveNumber.php" method = "POST">
-      <label for="Name">Enter your name: </label>
-      <input type="text" name="Name" placeholder="Enter Your Name"><br>
-      <label for="countrycode">Enter country code and phone number: </label>
-      <input type="text" name="countrycode" value="+" style="width:3em">
-      <input type="text" name="PhoneNumber" placeholder="Phone Number"><br>
-      <input type="submit" value="Submit" name="submit">
-    </form> -->
     <div class="form-style-8">
   <h2>SignUp for Reminders</h2>
   <form action = "saveNumber.php" method = "POST">
